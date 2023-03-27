57265b42dd35cd3397762a6de89295e8923f5a9e
sunilkumar_seenivasan <100457242+sunil52sk@users.noreply.github.com>
2023-03-27 14:13:06 +0530

    Add files via upload

---
+++ src/Component/Smile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Smile() {
+  return (
+    <div>
+        <h1>Smile Component</h1>
+        <p>it is a functional component</p>
+        <img src="https://www.shutterstock.com/image-vector/smile-icon-logo-vector-template-260nw-725486938.jpg" alt=''></img>
+    </div>
+  )
+}
+
+export default Smile
